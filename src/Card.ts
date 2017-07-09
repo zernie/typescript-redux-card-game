425e6df33e73f18d1af97c58dc36a968fbb01bf2
zernie <zergetaev@gmail.com>
2017-07-09 14:57:28 +0300

    Added CardType enum.

--- src/Card.ts
+++ src/Card.ts
@@ -1,9 +1,16 @@
+export enum CardType {
+  Minion,
+  Spell,
+  Weapon,
+}
+
 interface Card {
   cost: number;
   name: string;
   desc?: string;
   health: number;
   damage: number;
+  type: CardType;
 }
 
 export default Card;
