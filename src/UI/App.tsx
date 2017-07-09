c116a0de3e0af857e4dc4e2c868190451da45e8a
zernie <zergetaev@gmail.com>
2017-07-09 14:46:25 +0300

    Convert App to a stateless component.

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,25 +1,21 @@
 import * as React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 import BoardContainer from './BoardContainer';
+import { StatelessComponent } from 'react';
 
-class App extends React.Component<{}, {}> {
-  render() {
-    return (
-      <Grid container>
-        <Grid.Row centered>
-          <Header textAlign="center">
-            Hearthstone-inspired card game written using React and Redux.
-          </Header>
-        </Grid.Row>
+const App: StatelessComponent<{}> = props =>
+  <Grid container>
+    <Grid.Row centered>
+      <Header textAlign="center">
+        Hearthstone-inspired card game written using React and Redux.
+      </Header>
+    </Grid.Row>
 
-        <Grid.Row >
-          <div className="column">
-            <BoardContainer/>
-          </div>
-        </Grid.Row>
-      </Grid>
-    );
-  }
-}
+    <Grid.Row>
+      <div className="column">
+        <BoardContainer />
+      </div>
+    </Grid.Row>
+  </Grid>;
 
 export default App;
