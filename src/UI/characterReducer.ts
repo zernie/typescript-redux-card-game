74bac9b577db467e5fd6587e791f9a54f1e6a4e0
zernie <zergetaev@gmail.com>
2017-07-15 23:37:26 +0300

    Refactored characterReducer.

--- src/UI/characterReducer.ts
+++ src/UI/characterReducer.ts
@@ -1,7 +1,7 @@
 import Player, { ActivePlayer } from '../Player';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
-import {  inc, subtract, evolve } from 'ramda';
+import { inc, subtract, evolve } from 'ramda';
 import { nextTurn } from './turnReducer';
 
 const actionCreator = actionCreatorFactory();
@@ -14,6 +14,7 @@ interface AttackFaceAction {
 
 export const attackFace = actionCreator<AttackFaceAction>('ATTACK_FACE');
 
+// TODO: refactor
 const attackFaceHandler = (state: Player, payload: AttackFaceAction) =>
   evolve(
     {
@@ -22,13 +23,9 @@ const attackFaceHandler = (state: Player, payload: AttackFaceAction) =>
     state
   );
 
-const nextTurnHandler = (state: Player, payload: Object) =>
-  evolve(
-    {
-      totalMana: inc,
-    },
-    state
-  );
+const nextTurnHandler = evolve<Player>({
+  totalMana: inc,
+});
 
 const characterReducer = (character: Player) =>
   reducerWithInitialState<Player>(character)
