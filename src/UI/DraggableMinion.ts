b957e123fcdd0ec82fc1b2a83cfd2f084e7e293f
zergetaev <zergetaev@gmail.com>
2017-11-04 15:06:19 +0300

    Fix minion attack.

--- src/UI/DraggableMinion.ts
+++ src/UI/DraggableMinion.ts
@@ -11,16 +11,13 @@ const collect: DnD.DragSourceCollector = (connector, monitor) => ({
 const spec: DnD.DragSourceSpec<MinionProps> = {
   beginDrag: (props, monitor, component) => props,
   isDragging: (props, monitor: DnD.DragSourceMonitor) => monitor.isDragging(),
-  canDrag: (props, monitor: DnD.DragSourceMonitor) =>
-    props.minion.owner === props.activePlayer,
+  canDrag: (props, monitor: DnD.DragSourceMonitor) => props.minion.owner === props.state.activePlayer,
 };
 
 const DraggableMinion = DnD.DragSource<MinionProps>('Minion', spec, collect)(
   MinionCard
 );
 
-const mapStateToProps = R.pick(['activePlayer']);
+const mapStateToProps = R.pick(['state']);
 
-export default connect(mapStateToProps, {})(DraggableMinion) as ComponentClass<
-  Partial<MinionProps>
->;
+export default connect(mapStateToProps, {})(DraggableMinion) as ComponentClass<Partial<MinionProps>>;
--- src/UI/Minion.tsx
+++ src/UI/Minion.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { StatelessComponent } from 'react';
 import { List } from 'semantic-ui-react';
 import { Minion } from '../Minion';
-import { PlayerKind } from '../Player';
+import { State } from '../Game';
 
 export interface MinionProps {
-  activePlayer: PlayerKind;
+  state: State;
   connectDragSource: Function;
   isDragging: boolean;
   minion: Minion;
