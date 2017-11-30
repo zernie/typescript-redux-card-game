a1d8c38c524037a3a4c5ba2e96f67a7362fb2773
zergetaev <zergetaev@gmail.com>
2017-11-30 18:36:00 +0300

    Refactor shouldExhaust helper.

--- src/Character.ts
+++ src/Character.ts
@@ -12,26 +12,24 @@ export type CharacterContainer = Container<Character>;
 
 export const getCharacter = (id: number, game: Game): Character =>
   getCharacters(game)[id];
-
 export const getCharacters = (game: Game): CharacterContainer =>
   R.filter(
     R.propSatisfies(R.contains(R.__, [CardType.Minion, CardType.Hero]), 'type'),
     game.entities
   );
 
+export const canAttack = (character: Character): boolean =>
+  character.attack > 0 && !character.exhausted;
+
 export const hasAbility = R.curry(
   (ability: Ability, entity: Character | Card): boolean =>
     R.contains(ability, entity.abilities)
 );
-export const canAttack = (character: Character): boolean =>
-  character.attack > 0 && !character.exhausted;
-
-export const shouldExhaust = (character: Character): boolean =>
-  !(
-    character.attacksPerformed < 1 ||
-    (hasWindfury(character) && character.attacksPerformed < 2)
-  );
-
 export const hasTaunt = hasAbility(Ability.Taunt);
 export const hasCharge = hasAbility(Ability.Charge);
 export const hasWindfury = hasAbility(Ability.Windfury);
+
+export const shouldExhaust = (character: Character): boolean =>
+  hasWindfury(character)
+    ? character.attacksPerformed >= 2
+    : character.attacksPerformed >= 1;
