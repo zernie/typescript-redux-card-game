3360c909e735df198cdeefe3f0710e93e9319157
zergetaev <zergetaev@gmail.com>
2019-12-22 23:01:58 +0300

    Fix Deck

--- src/UI/Deck/Deck.tsx
+++ src/UI/Deck/Deck.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { Segment, Statistic } from "semantic-ui-react";
+import _ from "lodash/fp";
 import { CardContainer } from "../../Card";
-import { size } from "../../utils";
+import { SemanticCOLORS } from 'semantic-ui-react/dist/commonjs/generic';
 
 export interface SideProps {
   deck: CardContainer;
 }
 
-const deckColor = (deck: CardContainer) => (size(deck) > 0 ? "blue" : "red");
+const deckColor = (deck: CardContainer): SemanticCOLORS => (_.size(deck) > 0 ? "blue" : "red");
 
 const Deck: React.FunctionComponent<SideProps> = ({ deck }) => (
   <Segment basic>
-    <Statistic color={deckColor(deck)} value={size(deck)} label="Cards" />
+    <Statistic color={deckColor(deck)} value={_.size(deck)} label="Cards" />
   </Segment>
 );
 
