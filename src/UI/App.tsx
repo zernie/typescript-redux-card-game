e5e57d30bf472419b9cd1364b2fec16096e6d5bb
zergetaev <zergetaev@gmail.com>
2019-12-24 21:18:27 +0300

    Update header

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { DndProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
-import { Container, Header } from "semantic-ui-react";
+import {
+  Container,
+  Grid,
+  Header,
+  GridRow,
+  GridColumn
+} from "semantic-ui-react";
 import TargetableBattlefield from "./Battlefield";
 
 const App: React.FC = props => (
-  <Container textAlign="center">
-    <Header as="h1">
-      <Header.Content>
-        Hearthstone-like card game written using React and Redux.
-      </Header.Content>
-    </Header>
+  <Grid>
+    <GridRow>
+      <GridColumn>
+        <Container textAlign="center">
+          <Header as="h1" attached={true}>
+            <Header.Content
+              as={"a"}
+              href={"https://github.com/zernie/typescript-redux-card-game"}
+              target="_blank"
+            >
+              Hearthstone clone written using React and Redux.
+            </Header.Content>
+          </Header>
 
-    <DndProvider backend={HTML5Backend}>
-      <TargetableBattlefield />
-    </DndProvider>
-  </Container>
+          <DndProvider backend={HTML5Backend}>
+            <TargetableBattlefield />
+          </DndProvider>
+        </Container>
+      </GridColumn>
+    </GridRow>
+  </Grid>
 );
 
 export default App;
