436ab13ac53e4f9af05c855fad3090c43c6d25bb
zergetaev <zergetaev@gmail.com>
2019-12-28 20:54:57 +0300

    Add charsFromContainer, isCharacter helpers

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,10 +1,11 @@
+import _ from "lodash/fp";
 import { hasWindfury } from "./Card";
 import { CardType } from "./enums";
 import { Game } from "./Game";
 import { Hero } from "./Hero";
 import { Minion } from "./Minion";
 import { Entity, EntityContainer } from "./Entity";
-import { MinionContainer } from "./Container";
+import { CharacterContainer, } from "./Container";
 
 export type Character = Hero | Minion;
 
@@ -21,6 +22,9 @@ export const isHero = (entity: Entity): entity is Hero =>
   entity.type === CardType.Hero;
 export const isMinion = (entity: Entity): entity is Minion =>
   entity.type === CardType.Minion;
+export const isCharacter = (entity: Entity): entity is Character =>
+  isMinion(entity) || isHero(entity);
+
 
 // export const getCharacters = (game: Game): CharacterContainer =>
 //   _.filter(
@@ -35,3 +39,6 @@ export const shouldExhaust = (character: Character): boolean =>
   hasWindfury(character)
     ? character.attacksPerformed >= 2
     : character.attacksPerformed >= 1;
+
+export const charsFromContainer = (entities: EntityContainer) =>
+  _.pickBy(isCharacter, entities) as CharacterContainer;
