b40b321defa28e5d592ff9dfa58afc8494e67f3c
zernie <zergetaev@gmail.com>
2020-01-09 22:04:42 +0300

    Updates

---
+++ precache-manifest.f703da800002d41b3b41b0c1dc16e4ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b2ed96914ac0896e0d2affe7fdac16",
+    "url": "/typescript-redux-card-game/index.html"
+  },
+  {
+    "revision": "57e5fbb7e55d7b99f97c",
+    "url": "/typescript-redux-card-game/static/css/2.b7633113.chunk.css"
+  },
+  {
+    "revision": "57e5fbb7e55d7b99f97c",
+    "url": "/typescript-redux-card-game/static/js/2.56a7d0cf.chunk.js"
+  },
+  {
+    "revision": "b47ce9563e47f9bed842e028db97e090",
+    "url": "/typescript-redux-card-game/static/js/2.56a7d0cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1979fa3c4dd918ff33ce",
+    "url": "/typescript-redux-card-game/static/js/main.837683ec.chunk.js"
+  },
+  {
+    "revision": "6fbc49ec452591ce518b",
+    "url": "/typescript-redux-card-game/static/js/runtime-main.7e1f53e9.js"
+  },
+  {
+    "revision": "d534b9eeb0587280caed251c6bbe907e",
+    "url": "/typescript-redux-card-game/static/media/cardback.d534b9ee.png"
+  }
+]);
